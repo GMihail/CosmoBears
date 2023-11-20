06d9700c0dc9f54b0b6fe3f254b8cea14e96fc2f
GMihail <118734496+GMihail@users.noreply.github.com>
2023-11-20 23:26:13 +0400

    Update

---
+++ Assets/App/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Misha
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+        [SerializeField] private GameObject _MainMenu;
+        [SerializeField] private GameObject _SettingsMenu;
+
+    }
+}
