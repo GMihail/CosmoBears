a33538a94fea3e32eec09c1c293a03242a381945
GMihail <118734496+GMihail@users.noreply.github.com>
2023-11-21 20:46:35 +0400

    UI_Update

--- Assets/App/Scripts/ButtonManager.cs
+++ Assets/App/Scripts/ButtonManager.cs
@@ -1,13 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Misha
 {
-    public class NewBehaviourScript : MonoBehaviour
+    public class ButtonManager : MonoBehaviour
     {
-        [SerializeField] private GameObject _MainMenu;
-        [SerializeField] private GameObject _SettingsMenu;
+        //number 0 - MainMenu
+        //number 1 - SettingsMenu
+        //number 2 - PauseMenu
 
+        [SerializeField] private GameObject[] _Windows;
+        [SerializeField] private int _ActiveWindow;
+        [SerializeField] private int _PreviousWindow;
+        public Dropdown _DropDown;
+
+        
+        public void DropDownSettings()
+        {
+            if (_DropDown.value == 0)
+            {
+                Screen.SetResolution (1920, 1080, true);
+            }
+        }
+        public void ActivePreviousWindow()
+            {
+                if(_Windows[0].activeSelf == true)
+                {
+                    _ActiveWindow = 0;
+                }
+                if(_Windows[1].activeSelf == true)
+                {
+                    _ActiveWindow = 1;
+                }
+                if(_Windows[2].activeSelf == true)
+                {
+                    _ActiveWindow = 2;
+                }
+            }
+        public void SettingsButton()
+        {
+            _Windows[1].SetActive(true);
+            _Windows[_ActiveWindow].SetActive(false);
+            _PreviousWindow = _ActiveWindow;
+        }
+        public void BackButton()
+        {
+            _Windows[_PreviousWindow].SetActive(true);
+            _Windows[_ActiveWindow].SetActive(false);
+        }
+        
     }
 }
