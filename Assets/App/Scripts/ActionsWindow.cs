f52ebdc23d8c632f20cfdb54f7a01b9ceee409dc
GMihail <118734496+GMihail@users.noreply.github.com>
2023-11-23 23:23:15 +0400

    init

--- Assets/App/Scripts/ActionsWindow.cs
+++ Assets/App/Scripts/ActionsWindow.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 public class ActionsWindow : MonoBehaviour
 {
     [SerializeField] private GameObject _actionsMenu;
+    [SerializeField] private Slider _openSlider;
+    [SerializeField] private Transform _door;
+    [SerializeField] private float _offsetY;
+
+    private void Start()
+    {
+        _door = GetComponent<Transform>();
+        _openSlider.minValue = _door.position.y;
+        _openSlider.maxValue = _door.position.y + _offsetY;
+    }
     private void OnTriggerStay(Collider other)
     {
         if(Input.GetKeyDown(KeyCode.E)) Press();
@@ -18,10 +28,21 @@ public class ActionsWindow : MonoBehaviour
     public void Press()
     {
         _actionsMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
     }
     public void Out()
     {
         _actionsMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
     }
-    
+    public void OpenSlider()
+    {
+        _openSlider.gameObject.SetActive(true);
+    }
+    public void DoorOpen()
+    {
+        _door.position = new Vector3(transform.position.x, _openSlider.value, transform.position.z);
+    }
+
+
 }
