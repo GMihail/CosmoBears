018034b989ff1ceebddc6717059ca3cb7275dace
GMihail <118734496+GMihail@users.noreply.github.com>
2023-11-30 22:37:05 +0400

    UI

--- Assets/App/Scripts/ButtonManager.cs
+++ Assets/App/Scripts/ButtonManager.cs
@@ -16,29 +16,33 @@ namespace Misha
         [SerializeField] private int _PreviousWindow;
         public Dropdown _DropDown;
 
-        
+
         public void DropDownSettings()
         {
             if (_DropDown.value == 0)
             {
-                Screen.SetResolution (1920, 1080, true);
+                Screen.SetResolution(1920, 1080, true);
             }
         }
         public void ActivePreviousWindow()
+        {
+            if (_Windows[0].activeSelf == true)
             {
-                if(_Windows[0].activeSelf == true)
-                {
-                    _ActiveWindow = 0;
-                }
-                if(_Windows[1].activeSelf == true)
-                {
-                    _ActiveWindow = 1;
-                }
-                if(_Windows[2].activeSelf == true)
-                {
-                    _ActiveWindow = 2;
-                }
+                _ActiveWindow = 0;
             }
+            if (_Windows[1].activeSelf == true)
+            {
+                _ActiveWindow = 1;
+            }
+            if (_Windows[2].activeSelf == true)
+            {
+                _ActiveWindow = 2;
+            }
+        }
+        public void GameMenu()
+        {
+
+        }
         public void SettingsButton()
         {
             _Windows[1].SetActive(true);
@@ -50,6 +54,27 @@ namespace Misha
             _Windows[_PreviousWindow].SetActive(true);
             _Windows[_ActiveWindow].SetActive(false);
         }
-        
+        public void PauseButton()
+        {
+            _Windows[2].SetActive(true);
+            _Windows[_ActiveWindow].SetActive(false);
+            _PreviousWindow = _ActiveWindow;
+        }
+        public void MainMenuButton()
+        {
+            _Windows[0].SetActive(true);
+            _Windows[_ActiveWindow].SetActive(false);
+            _PreviousWindow = _ActiveWindow;
+        }
+        public void SaveGame()
+        {
+
+        }
+        public void LoadGame()
+        {
+
+        }
+
+
     }
 }
