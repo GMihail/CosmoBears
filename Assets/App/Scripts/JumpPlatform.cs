109044b019b382eb7bf11307bcaec10f4c7564f9
GMihail <118734496+GMihail@users.noreply.github.com>
2023-12-04 16:54:54 +0400

    JumpPlatformUpdate

--- Assets/App/Scripts/JumpPlatform.cs
+++ Assets/App/Scripts/JumpPlatform.cs
@@ -1,46 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
 
 public class JumpPlatform : MonoBehaviour
 {
-    public Rigidbody _rb;
-    public float _range = 1f;
-    public float _jumpAmount = 35;
-    public bool _grounded = false;
-    public Camera _camera;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody _player;
+    public Transform _referencePoint;
+    private Vector3 _jumpVector;
+    public float _powerJump;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        ShootRayCast();
-        if (Input.GetKeyDown(KeyCode.Space) & _grounded == true)
-        {
-            _rb.AddForce(Vector3.up * _jumpAmount, ForceMode.Impulse);
-        }
-    }
-    private void ShootRayCast()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, _range))
-        {
-            Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
-            if(target != null)
-            {
-                StartCoroutine(JumpBool());
-            }
-        }
-    }
-    IEnumerator JumpBool()
-    {
-        _grounded = true;
-        yield return new WaitForSeconds(0.5f);
-        _grounded = false;
+        _jumpVector = _referencePoint.transform.position - _player.transform.position;
+        _player.AddForce(_jumpVector * _powerJump, ForceMode.Impulse);
     }
 }
